e8919ee2b975cf19960560db22b730e87aee8dcb
Kukac <thekukac9@gmail.com>
2018-04-30 23:57:14 +0200

    init
    init

---
+++ hex to bin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hex_to_bin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var list = new List<byte> { 0x1A, 0x0F, 0x83 }.ToArray();
+
+            using (var s = File.Open("test.bin", FileMode.Create))
+            using (var tw = new BinaryWriter(s))
+                tw.Write(list);
+
+        }
+    }
+}
